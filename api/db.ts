ca38647c86eb45713e987148ffbf2deb02c90c6d
SANTO BRIANA <santobriana.16@gmail.com>
2025-03-26 10:17:44 -0700

    Update db.ts

--- api/db.ts
+++ api/db.ts
@@ -2,7 +2,6 @@ import { Pool, neonConfig } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-serverless";
 import ws from "ws";
 import * as schema from "@shared/schema";
-
 neonConfig.webSocketConstructor = ws;
 
 if (!process.env.DATABASE_URL) {
